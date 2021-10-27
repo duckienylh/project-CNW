607387978399ef0be788291792b066ca0836e9a4
ChuongDuc <ducchuong2k@gmail.com>
2021-10-27 23:55:28 +0700

    fix header

--- admin/headerad.php
+++ admin/headerad.php
@@ -7,40 +7,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
-    <link rel="stylesheet" href="../asset/style.css">
-    <title>Quản Lý Học Sinh</title>
+    <title>Quản lý học sinh THPT</title>
 </head>
-
 <body>
     <div class="container-fluid">
-        <div class="row ">
-            <div>
-                <ul class="nav justify-content-center border-bottom border-2 border-dark">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="dashboard.php">Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="class.php">Class</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="subject.php">Subject</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="teacher.php">Teacher</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="result.php">Result</a>
-                    </li>
-                    <li class="nav-item dropdown ">
-                        <a class="nav-link  dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            User
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarDarkDropdownMenuLink">
-                            <li><a class="dropdown-item" href="#">Manage Account</a></li>
-                            <li><a class="dropdown-item" href="../index.php">Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
+        <div class="row bg-warning" >
+            <div class="col-md-6 d-flex" style="height: 64px">
+                
+                <h5 class="align-self-md-center" >Quản lý học sinh</h5>
             </div>
+            <div class="col-md-6 ">
+                <nav class="navbar navbar-expand-lg navbar-light float-end z-1 " style="height: 48px">
+                    <div class="me-4">
+                        <a href="#" class="text-decoration-none text-white"><i class="fas fa-user"></i> Tên giáo viên</a>
+                    </div>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            
+                            
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Học kỳ
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">2021-2022</a></li>
+                                    <li><a class="dropdown-item" href="#">2022-2023</a></li>
+                                    <li><a class="dropdown-item" href="#">2023-2024</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Đổi mật khẩu </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Thoát</a>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                </nav>
+            </div>
+        </div>
+        
         </div>
     </div>
