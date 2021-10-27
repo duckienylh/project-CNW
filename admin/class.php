d80785e8d4d159f893dbbdb1019ce0fe99f7432b
ChuongDuc <ducchuong2k@gmail.com>
2021-10-27 21:34:23 +0700

    update class

---
+++ admin/class.php
@@ -0,0 +1,48 @@
+<?php
+include '../reuse/header.php';
+?>
+<main>
+    <div class=" bg-info ">
+        <div class="container ">
+            <div class="row mt-3 pt-3">
+                <div class="col-md-6">
+                    <h2>Class</h2>
+                </div>
+                <div class="col-md-6">
+
+                    <a href="" class="btn btn-md btn-outline-light btn-primary float-end">
+                        <i class="fas fa-user-plus"></i> Add New Class
+                    </a>
+                </div>
+            </div>
+
+        </div>
+        <div class="container-fluid py-3">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Class ID</th>
+                        <th scope="col">Class Name</th>
+                        <th scope="col">Form Teacher</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>
+                            <a class="btn btn-warning" href=""><i class="fas fa-user-edit"></i></a>
+                            <a class="btn btn-danger" href=""><i class="fas fa-user-slash"></i></i></a>
+                        </td>
+                    </tr>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</main>
+<?php
+include '../reuse/footer.php';
+?>
--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,52 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
-    <link rel="stylesheet" href="../asset/style.css">
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="container-fluid">
-        <div class="row ">
-            <div>
-                <ul class="nav justify-content-center border-bottom border-2 border-dark">
-                    <li class="nav-item">
-                        <a class="nav-link " href="">Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="">Class</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="">Subject</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="">Student</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="">Result</a>
-                    </li>
-                    <li class="nav-item dropdown ">
-                        <a class="nav-link  dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            User
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Manage Account</a></li>
-                            <li><a class="dropdown-item" href="#">Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
+<?php
+    include '../reuse/header.php'
+?>
+<main>
     <div class=" bg-info ">
         <div class="container ">
-            <div class="row mt-3">
+            <div class="row mt-3 pt-3">
                 <h2>Home</h2>
             </div>
             <div class="row mt-3 justify-content-around py-5">
@@ -72,7 +30,7 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-
-</html>
+</main>
+<?php
+    include '../reuse/footer.php';
+?>
