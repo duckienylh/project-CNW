22bb88004bbf03235366c78575bc7b40746924af
duckienylh <duckienlh@gmail.com>
2021-10-27 16:28:38 +0700

    login

--- index.php
+++ index.php
@@ -1,9 +1,93 @@
-<?php
-    include './reuse/header.php';
-?>
-<main>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Quản lý học sinh THPT</title>
+</head>
+<style>
+    .gradient-custom-3 {
+        /* fallback for old browsers */
+        background: #84fab0;
+        /* Chrome 10-25, Safari 5.1-6 */
+        background: -webkit-linear-gradient(to right, rgba(132, 250, 176, 0.5), rgba(143, 211, 244, 0.5));
+        /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        background: linear-gradient(to right, rgba(132, 250, 176, 0.5), rgba(143, 211, 244, 0.5))
+    }
+
+    .gradient-custom-4 {
+        /* fallback for old browsers */
+        background: #84fab0;
+        /* Chrome 10-25, Safari 5.1-6 */
+        background: -webkit-linear-gradient(to right, rgba(132, 250, 176, 1), rgba(143, 211, 244, 1));
+        /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        background: linear-gradient(to right, rgba(132, 250, 176, 1), rgba(143, 211, 244, 1))
+    }
+</style>
+
+<body>
+    <main>
+        <form method="post">
+            <section class="vh-100 bg-image" style="background-image: url('https://mdbootstrap.com/img/Photos/new-templates/search-box/img4.jpg');">
+                <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+                    <div class="container h-100">
+                        <div class="row d-flex justify-content-center align-items-center h-100">
+                            <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+                                <div class="card" style="border-radius: 15px;">
+                                    <div class="card-body p-5">
+                                        <h2 class="text-uppercase text-center mb-5">account login</h2>
+                                        <form>
+
+                                            <div class="form-outline mb-4">
+                                                <input type="text" class="form-control form-control-lg" name="txtname">
+                                                <label class="form-label">user name</label>
+                                            </div>
+                                            <div class="form-outline mb-4">
+                                                <input type="text"  class="form-control form-control-lg" name="txtpass">
+                                                <label class="form-label">password</label>
+                                            </div>
+                                            <div class="d-flex justify-content-center">
+                                                <button type="submit" class="btn btn-success btn-block btn-lg gradient-custom-4 text-body" name="btnlogin">login</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </form>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     
-</main>
+
+</body>
+<!-- process login -->
 <?php
-    include './reuse/footer.php';
+session_start();
+if (isset($_POST['btnlogin'])) {
+    $username = $_POST['txtname'];
+    $password = $_POST['txtpass'];
+
+    $conn = mysqli_connect('localhost', 'root', '', 'danhba_dt');
+    if (!$conn) {
+        die("khong ket noi dc");
+    }
+
+    $sql = "SELECT *FROM db_users WHERE user_name = '$username' and user_pass = '$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $_SESSION['loginOK'] = $username;
+        header("location:./reuse/header.php");
+    } else {
+        header("location:./index.php");
+    }
+}
 ?>
+
+</html>
