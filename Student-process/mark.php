dc518c09d5dcceac8e7e425b5f9a909097833273
hoaiphuong123 <hp34nam@gmail.com>
2021-10-29 16:54:00 +0700

    update

--- Student-process/mark.php
+++ Student-process/mark.php
@@ -1,44 +1,7 @@
 <?php
 include './sidebar.php';
 ?>
-<!-- <main>
-    <div >
-        <div class="container ">
-            <div class="row  ">
-                <div class="col-md-6 mt-3">
-                    <h2>Mark</h2>
-                </div>
 
-            </div>
-
-        </div>
-        <div class="container-fluid py-3  ">
-            <table class="table table-hover  table-bordered border border-dark ">
-                <thead class="bg-dark">
-                    <tr style="color:white;">
-                        <th scope="col">Student Name</th>
-                        <th scope="col">Subject Name</th>
-                        <th scope="col">Mark Term</th>
-                        <th scope="col">Mark Hour Test</th>
-                        <th scope="col">Mark Final Exam</th>
-                        <th scope="col">Mark Medium Score</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Mark</td>
-                        <td>Nguyen Van A</td>
-                        <td>8</td>
-                        <td>7</td>
-                        <td>9</td>
-                        <td>8.2</td>
-                    </tr>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</main> -->
 <div class="col py-3">
     <main class="container-fluid" style="min-width: 759px;">
         <div class="d-flex justify-content-between bg-light ">
--- Student-process/student-index.php
+++ Student-process/student-index.php
@@ -2,42 +2,7 @@
 include './sidebar.php';
 ?>
 
-<!-- <div class="col py-3">
 
-    <div class="container ">
-        <div class="row  ">
-            <div class="col-md-6 mt-3">
-                <h2>Student</h2>
-            </div>
-
-        </div>
-
-    </div>
-    <div class="container-fluid py-3  ">
-        <table class="table table-hover  table-bordered border border-dark ">
-            <thead class="bg-dark">
-                <tr style="color:white;">
-                    <th scope="col">Student ID</th>
-                    <th scope="col">Student Name</th>
-                    <th scope="col">Student Gender</th>
-                    <th scope="col">Student Birth</th>
-                    <th scope="col">Student Address</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Nguyen Van A</td>
-                    <td>Nam</td>
-                    <td>2007-12-7</td>
-                    <td>Ha Noi</td>
-                </tr>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-
-</div> -->
 
 <div class="col py-3">
     <main class="container-fluid" style="min-width: 759px;">
