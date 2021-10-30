d76783afd3f3a67764ac54131079e87ecc6b988b
hoaiphuong123 <hp34nam@gmail.com>
2021-10-30 23:02:11 +0700

    update student

--- student/mark.php
+++ student/mark.php
@@ -15,57 +15,41 @@ include './sidebarst.php';
             <table class="table table-primary table-hover table-bordered">
                 <thead class=" text while">
                     <tr>
-                        <th scope="col">Mã Học sinh</th>
+                        <th scope="col">Mã học sinh</th>
                         <th scope="col">Mã môn học</th>
-                        <th scope="col">Điểm miệng</th>
                         <th scope="col">Điểm KT 1 tiết</th>
                         <th scope="col">Điểm cuối kỳ</th>
                         <th scope="col">Điểm trung bình</th>
-                        <th scope="col">Xếp loại</th>
                     </tr>
                 </thead>
                 <tbody>
                     <!--xuất dữ liệu theo CSDL -->
                     <?php
-                            //* B1: mở kết nối
-                            // include '../config.php';
-                            // //* B2: Truy vấn
-                            // $sql = "SELECT * FROM `ma` WHERE bd_id = '$id' ";
+                           // * B1: mở kết nối
+                            include '../config.php';
+                            //* B2: Truy vấn
+                            $sql = "SELECT * FROM marks";
 
-                            // //? lưu kết quả trả về $result
-                            // $result = mysqli_query($conn, $sql);
-                            // $gender;
+                            //? lưu kết quả trả về $result
+                            $result = mysqli_query($conn, $sql);
+                            $gender;
 
-                            // //* B3: Phân tích sử lý kết quả
-                            // if (mysqli_num_rows($result) > 0) {
-                            //     while ($row = mysqli_fetch_assoc($result)) {
-                            //         echo "<tr>";
-                            //         echo '<th scope="row">' . $row['bd_id'] . '</th>';
-                            //         echo '<td>' . $row['bd_name'] . '</td>';
-                            //         echo '<td>' . $row['bd_sex'] . '</td>';
-                            //         echo '<td>' . $row['bd_age'] . '</td>';
-                            //         echo '<td>' . $row['bd_bgroup'] . '</td>';
-                            //         echo '<td>' . $row['bd_reg_date'] . '</td>';
-                            //         echo '<td>' . $row['bd_phno'] . '</td>';
-                            //         echo '<td>
-                            //                 <a href="process/edit.php?id=' . $id . '" class="btn btn-primary">Sửa</a>
-                            //                 <a href="process/delete.php?id=' . $id . '" class="btn btn-danger">Xoá</a>
-                            //             </td>';
-                            //         echo '</tr>';
-                            //     }
-                            // }
-                            // //* B4: đóng kết nối
-                            // mysqli_close($conn);
+                            //* B3: Phân tích sử lý kết quả
+                            if (mysqli_num_rows($result) > 0) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr>";
+                                    echo '<td>' . $row['st_id'] . '</td>';
+                                    echo '<td>' . $row['sb_id'] . '</td>';
+                                    echo '<td>' . $row['ma_hour_test'] . '</td>';
+                                    echo '<td>' . $row['ma_final_exam'] . '</td>';
+                                    echo '<td>' . $row['ma_avarage'] . '</td>';
+                                    echo '</tr>';
+                                }
+                            }
+                            //* B4: đóng kết nối
+                            mysqli_close($conn);
                             ?>
-                    <!-- <tr>
-                        <td>Mark</td>
-                        <td>Nguyen Van A</td>
-                        <td>8</td>
-                        <td>7</td>
-                        <td>9</td>
-                        <td>8.2</td>
-
-                    </tr> -->
+                    
                 </tbody>
             </table>
         </div>
