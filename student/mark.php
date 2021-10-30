e27fc3261330cec71d06ff68983fa83a964dc153
hoaiphuong123 <hp34nam@gmail.com>
2021-10-30 15:38:33 +0700

    update student

--- student/mark.php
+++ student/mark.php
@@ -5,16 +5,16 @@ include './sidebarst.php';
 <div class="col py-3">
     <main class="container-fluid" style="min-width: 759px;">
         <div class="d-flex justify-content-between bg-light ">
-            <h2>Mark</h2>
+            <h2 class="text-primary">Mark</h2>
 
         </div>
 
 
         <div class="col-md-12" style="width:100% ; overflow: auto; height: auto">
 
-            <table class="table table-bordered">
-                <thead>
-                    <tr class="table-success">
+            <table class="table table-primary table-hover table-bordered">
+                <thead class=" text while">
+                    <tr >
                         <th scope="col">Student Name</th>
                         <th scope="col">Subject Name</th>
                         <th scope="col">Mark Term</th>
--- student/student-index.php
+++ student/student-index.php
@@ -7,34 +7,34 @@ include './sidebarst.php';
 <div class="col py-3">
     <main class="container-fluid" style="min-width: 759px;">
         <div class="d-flex justify-content-between bg-light ">
-        <h2>Student</h2>
+            <h2 class="text-primary">Student</h2>
 
         </div>
 
 
-        <div class="col-md-12" style="width:100% ; overflow: auto; height: auto">
+        <div class=" col-md-12" style="width:100% ; overflow: auto; height: auto">
 
-            <table class="table table-ronsponsive table-bordered">
-                <thead>
-                    <tr class="table-secondary ">
-                    <th scope="col">Student ID</th>
-                    <th scope="col">Student Name</th>
-                    <th scope="col">Student Gender</th>
-                    <th scope="col">Student Birth</th>
-                    <th scope="col">Student Address</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Nguyen Van A</td>
-                    <td>Nam</td>
-                    <td>2007-12-7</td>
-                    <td>Ha Noi</td>
+                <table class="table table-primary table-hover table-bordered">
+                    <thead class=" text while">
+                        <tr  >
+                            <th scope="col">Student ID</th>
+                            <th scope="col">Student Name</th>
+                            <th scope="col">Student Gender</th>
+                            <th scope="col">Student Birth</th>
+                            <th scope="col">Student Address</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Nguyen Van A</td>
+                            <td>Nam</td>
+                            <td>2007-12-7</td>
+                            <td>Ha Noi</td>
 
-                    </tr>
-                </tbody>
-            </table>
+                        </tr>
+                    </tbody>
+                </table>
         </div>
     </main>
 </div>
