4b3ef89a9134559d1f8a3963452a178d307d21fd
duckienylh <duckienlh@gmail.com>
2021-11-04 00:05:53 +0700

    fix student

--- student/mark.php
+++ student/mark.php
@@ -5,104 +5,103 @@ include "../config.php";
 ?>
 
 <div class="col py-3">
-    <main class="container-fluid" style="min-width: 759px;">
-        <div class="d-flex justify-content-between bg-light ">
-            <h2 class="text-primary mt-2">Kết quả</h2>
-            <form method="post">
-                <div class="row">
-                    <div class="col-md-6 mt-2">
-                        <div class="container-1">
-                            <!-- <span class="icon"><i class="fa fa-search"></i></span> -->
-                            <input type="text" class="form-control" name="search-name" placeholder="Search name...">
-                        </div>
+
+    <div class="d-flex justify-content-between bg-light ">
+        <h2 class="text-primary mt-2">Kết quả</h2>
+        <form method="post">
+            <div class="row">
+                <div class="col-md-6 mt-2">
+                    <div class="container-1">
+                        <!-- <span class="icon"><i class="fa fa-search"></i></span> -->
+                        <input type="text" class="form-control" name="search-name" placeholder="Search name...">
                     </div>
-                    <div class="col-md-3 mt-2 mb-1">
-                        <div class="form-group row ">
-                            <div class="col-sm-10 m ">
-                                <button type="submit" name="btnsearch" class="btn btn-primary">Search</button>
-                            </div>
+                </div>
+                <div class="col-md-3 mt-2 mb-1">
+                    <div class="form-group row ">
+                        <div class="col-sm-10 m ">
+                            <button type="submit" name="btnsearch" class="btn btn-primary">Search</button>
                         </div>
                     </div>
                 </div>
+            </div>
 
-            </form>
+        </form>
 
-        </div>
+    </div>
 
 
-        <div class="col-md-12" style="width:100% ; overflow: auto; height: auto">
+    <div class="col-md-12" style="width:100% ; overflow: auto; height: auto">
 
-            <table class="table table-primary table-hover table-bordered">
-                <thead class=" text while">
-                    <tr>
-                        <!-- <th class="text-center">#</th> -->
-                        <th>Mã học sinh</th>
-                        <th>Tên học sinh</th>
-                        <th>Tên môn học</th>
-                        <th>Điểm Kiểm tra 15p</th>
-                        <th>Điểm Kiểm tra 1 tiết</th>
-                        <th>Điểm cuối kỳ</th>
-                        <th>Điểm trung bình</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <!--xuất dữ liệu theo CSDL -->
-                    <?php
-                           // * B1: mở kết nối
-                            include '../config.php';
-                            if (isset($_POST['btnsearch'])){
-                                $id_name = $_POST['search-name'];
-                               
-                                 //* B2: Truy vấn
-                                $sql = "SELECT st.st_id,st.st_name,sb.sb_name,m.ma_mini_test,m.ma_hour_test,m.ma_final_exam,m.ma_avarage 
+        <table class="table table-primary table-hover table-bordered">
+            <thead class=" text while">
+                <tr>
+                    <!-- <th class="text-center">#</th> -->
+                    <th>Mã học sinh</th>
+                    <th>Tên học sinh</th>
+                    <th>Tên môn học</th>
+                    <th>Điểm Kiểm tra 15p</th>
+                    <th>Điểm Kiểm tra 1 tiết</th>
+                    <th>Điểm cuối kỳ</th>
+                    <th>Điểm trung bình</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!--xuất dữ liệu theo CSDL -->
+                <?php
+                // * B1: mở kết nối
+                include '../config.php';
+                if (isset($_POST['btnsearch'])) {
+                    $id_name = $_POST['search-name'];
+
+                    //* B2: Truy vấn
+                    $sql = "SELECT st.st_id,st.st_name,sb.sb_name,m.ma_mini_test,m.ma_hour_test,m.ma_final_exam,m.ma_avarage 
                                 FROM marks m, students st, subjects sb 
                                 WHERE m.st_id=st.st_id and m.sb_id=sb.sb_id and st.st_name like'%$id_name%'";
-                                 //? lưu kết quả trả về $result
-                                $result = mysqli_query($conn, $sql);
-                                //* B3: Phân tích sử lý kết quả
-                                if (mysqli_num_rows($result) > 0) {
-                                    while ($row = mysqli_fetch_assoc($result)) {
-                                        echo "<tr>";
-                                        echo '<td>' . $row['st_id'] . '</td>';
-                                        echo '<td>' . $row['st_name'] . '</td>';
-                                        echo '<td>' . $row['sb_name'] . '</td>';
-                                        echo '<td>' . $row['ma_mini_test'] . '</td>';
-                                        echo '<td>' . $row['ma_hour_test'] . '</td>';
-                                        echo '<td>' . $row['ma_final_exam'] . '</td>';
-                                        echo '<td>' . $row['ma_avarage'] . '</td>';
-                                        echo '</tr>';
-                                    }
-                                }
-                                }
-                                else {
-                                    $sql = "SELECT st.st_id,st.st_name,sb.sb_name,m.ma_mini_test,m.ma_hour_test,m.ma_final_exam,m.ma_avarage 
+                    //? lưu kết quả trả về $result
+                    $result = mysqli_query($conn, $sql);
+                    //* B3: Phân tích sử lý kết quả
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo '<td>' . $row['st_id'] . '</td>';
+                            echo '<td>' . $row['st_name'] . '</td>';
+                            echo '<td>' . $row['sb_name'] . '</td>';
+                            echo '<td>' . $row['ma_mini_test'] . '</td>';
+                            echo '<td>' . $row['ma_hour_test'] . '</td>';
+                            echo '<td>' . $row['ma_final_exam'] . '</td>';
+                            echo '<td>' . $row['ma_avarage'] . '</td>';
+                            echo '</tr>';
+                        }
+                    }
+                } else {
+                    $sql = "SELECT st.st_id,st.st_name,sb.sb_name,m.ma_mini_test,m.ma_hour_test,m.ma_final_exam,m.ma_avarage 
                                     FROM marks m, students st, subjects sb 
                                     WHERE m.st_id=st.st_id and m.sb_id=sb.sb_id";
-                                      //? lưu kết quả trả về $result
-                                    $result = mysqli_query($conn, $sql);
-                                    //* B3: Phân tích sử lý kết quả
-                                        if (mysqli_num_rows($result) > 0) {
-                                        while ($row = mysqli_fetch_assoc($result)) {
-                                            echo "<tr>";
-                                            echo '<td>' . $row['st_id'] . '</td>';
-                                            echo '<td>' . $row['st_name'] . '</td>';
-                                            echo '<td>' . $row['sb_name'] . '</td>';
-                                            echo '<td>' . $row['ma_mini_test'] . '</td>';
-                                            echo '<td>' . $row['ma_hour_test'] . '</td>';
-                                            echo '<td>' . $row['ma_final_exam'] . '</td>';
-                                            echo '<td>' . $row['ma_avarage'] . '</td>';
-                                            echo '</tr>';
-                                        }
-                                    }
-                                }
-                            //* B4: đóng kết nối
-                            mysqli_close($conn);
-                            ?>
+                    //? lưu kết quả trả về $result
+                    $result = mysqli_query($conn, $sql);
+                    //* B3: Phân tích sử lý kết quả
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo '<td>' . $row['st_id'] . '</td>';
+                            echo '<td>' . $row['st_name'] . '</td>';
+                            echo '<td>' . $row['sb_name'] . '</td>';
+                            echo '<td>' . $row['ma_mini_test'] . '</td>';
+                            echo '<td>' . $row['ma_hour_test'] . '</td>';
+                            echo '<td>' . $row['ma_final_exam'] . '</td>';
+                            echo '<td>' . $row['ma_avarage'] . '</td>';
+                            echo '</tr>';
+                        }
+                    }
+                }
+                //* B4: đóng kết nối
+                mysqli_close($conn);
+                ?>
 
-                </tbody>
-            </table>
-        </div>
-    </main>
+            </tbody>
+        </table>
+    </div>
+
 </div>
 
 <?php
--- student/student-index.php
+++ student/student-index.php
@@ -5,104 +5,100 @@ include './sidebarst.php';
 
 
 <div class="col py-3">
-    <main class="container-fluid" style="min-width: 759px;">
-        <div class="d-flex justify-content-between bg-light ">
-            <div class="col-md-6 mt-2">
-                <h2 class="text-primary">Học Sinh</h2>
-            </div>
-            <form method="post">
-                <div class="row">
-                    <div class="col-md-6 mt-2">
-                        <div class="container-1">
-                            <!-- <span class="icon"><i class="fa fa-search"></i></span> -->
-                            <input type="text" class="form-control" name="search-idstudent" placeholder="Search ID...">
-                        </div>
-                    </div>
-                    <div class="col-md-3 mt-2 ">
-                        <div class="form-group row ">
-                            <div class="col-sm-10 ">
-                                <button type="submit" name="btnsearch" class="btn btn-primary">Search</button>
-                            </div>
-                        </div>
-                    </div>
+
+    <div class="d-flex justify-content-between bg-light ">
+        <div class="col-md-6 mt-2">
+            <h2 class="text-primary">Học Sinh</h2>
+        </div>
+        <form method="post">
+            <div class="row">
+                <div class="col-md-6 mt-2 d-flex">
+
+                    <!-- <span class="icon"><i class="fa fa-search"></i></span> -->
+                    <input type="text" class="form-control col-md-6 me-3" name="search-idstudent" placeholder="Search ID...">
+                    <button type="submit" name="btnsearch" class="btn btn-primary">Search</button>
+
                 </div>
 
-            </form>
+            </div>
 
-        </div>
+        </form>
+
+    </div>
 
 
-        <div class=" col-md-12" style="width:100% ; overflow: auto; height: auto">
-            <table class="table table-primary table-hover table-bordered ">
-                <thead class=" text while">
-                    <tr>
-                        <th scope="col">Mã học sinh</th>
-                        <th scope="col">Tên học sinh</th>
-                        <th scope="col">Ngày sinh</th>
-                        <th scope="col">Địa chỉ</th>
-                        <th scope="col">Giới tính</th>
-                        <th scope="col">Số điện thoại</th>
-                        <th scope="col">Tên phụ huynh</th>
-                        <th scope="col">Mã lớp</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
+    <div class="col-md-12" style="width:100% ; overflow: auto; height: auto">
 
-                           // * B1: mở kết nối
-                            include '../config.php';       
-                            if (isset($_POST['btnsearch'])){
-                                $id_student = $_POST['search-idstudent'];
-                                 $sql = "SELECT * FROM `students` WHERE st_id ='$id_student'";
-                                  //* B2: Truy vấn
-                                //? lưu kết quả trả về $result
-                                $result = mysqli_query($conn, $sql);
-                               
-                                //* B3: Phân tích sử lý kết quả
-                                if (mysqli_num_rows($result) > 0) {
-                                    while ($row = mysqli_fetch_assoc($result)) {
-                                        echo '<tr>';
-                                        echo '<td>' . $row['st_id'] . '</td>';
-                                        echo '<td>' . $row['st_name'] . '</td>';
-                                        echo '<td>' . $row['st_birth'] . '</td>';
-                                        echo '<td>' . $row['st_address'] . '</td>';
-                                        echo '<td>' . $row['st_gender'] . '</td>';
-                                        echo '<td>' . $row['st_phone'] . '</td>';
-                                        echo '<td>' . $row['st_parent'] . '</td>';
-                                        echo '<td>' . $row['class_id'] . '</td>';
-                                        echo '</tr>';
-                                    }
-                                }
-                            }else {
-                                // $id_student = $_POST['search-idstudent'];
-                                $sql = "SELECT * FROM `students`";
-                                //* B2: Truy vấn
-                              //? lưu kết quả trả về $result
-                              $result = mysqli_query($conn, $sql);
-                            //   echo $id_student;
-                              //* B3: Phân tích sử lý kết quả
-                              if (mysqli_num_rows($result) > 0) {
-                                  while ($row = mysqli_fetch_assoc($result)) {
-                                    echo '<tr>';
-                                      echo '<td>' . $row['st_id'] . '</td>';
-                                      echo '<td>' . $row['st_name'] . '</td>';
-                                      echo '<td>' . $row['st_birth'] . '</td>';
-                                      echo '<td>' . $row['st_address'] . '</td>';
-                                      echo '<td>' . $row['st_gender'] . '</td>';
-                                      echo '<td>' . $row['st_phone'] . '</td>';
-                                      echo '<td>' . $row['st_parent'] . '</td>';
-                                      echo '<td>' . $row['class_id'] . '</td>';
-                                      echo '</tr>';
-                                  }
-                              }
-                            }
-                            //* B4: đóng kết nối
-                            mysqli_close($conn);
-                    ?>
-                </tbody>
-            </table>
-        </div>
-    </main>
+        <table class="table table-bordered">
+            <thead>
+                <tr style="background-color: #9ec5fe;">
+                    <th scope="col">Mã học sinh</th>
+                    <th scope="col">Tên học sinh</th>
+                    <th scope="col">Ngày sinh</th>
+                    <th scope="col">Địa chỉ</th>
+                    <th scope="col">Giới tính</th>
+                    <th scope="col">Số điện thoại</th>
+                    <th scope="col">Tên phụ huynh</th>
+                    <th scope="col">Mã lớp</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+
+                // * B1: mở kết nối
+                include '../config.php';
+                if (isset($_POST['btnsearch'])) {
+                    $id_student = $_POST['search-idstudent'];
+                    $sql = "SELECT * FROM `students` WHERE st_id ='$id_student'";
+                    //* B2: Truy vấn
+                    //? lưu kết quả trả về $result
+                    $result = mysqli_query($conn, $sql);
+
+                    //* B3: Phân tích sử lý kết quả
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo '<tr>';
+                            echo '<td>' . $row['st_id'] . '</td>';
+                            echo '<td>' . $row['st_name'] . '</td>';
+                            echo '<td>' . $row['st_birth'] . '</td>';
+                            echo '<td>' . $row['st_address'] . '</td>';
+                            echo '<td>' . $row['st_gender'] . '</td>';
+                            echo '<td>' . $row['st_phone'] . '</td>';
+                            echo '<td>' . $row['st_parent'] . '</td>';
+                            echo '<td>' . $row['class_id'] . '</td>';
+                            echo '</tr>';
+                        }
+                    }
+                } else {
+                    // $id_student = $_POST['search-idstudent'];
+                    $sql = "SELECT * FROM `students`";
+                    //* B2: Truy vấn
+                    //? lưu kết quả trả về $result
+                    $result = mysqli_query($conn, $sql);
+                    //   echo $id_student;
+                    //* B3: Phân tích sử lý kết quả
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo '<tr>';
+                            echo '<td>' . $row['st_id'] . '</td>';
+                            echo '<td>' . $row['st_name'] . '</td>';
+                            echo '<td>' . $row['st_birth'] . '</td>';
+                            echo '<td>' . $row['st_address'] . '</td>';
+                            echo '<td>' . $row['st_gender'] . '</td>';
+                            echo '<td>' . $row['st_phone'] . '</td>';
+                            echo '<td>' . $row['st_parent'] . '</td>';
+                            echo '<td>' . $row['class_id'] . '</td>';
+                            echo '</tr>';
+                        }
+                    }
+                }
+                //* B4: đóng kết nối
+                mysqli_close($conn);
+                ?>
+            </tbody>
+        </table>
+    </div>
+
 </div>
 <?php
 include './foorterst.php';
